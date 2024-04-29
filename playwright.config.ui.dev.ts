af23a50d9ddcd597ae039129d77df5e599e2eef6
Pipeline <pipeline@demo.com>
2024-04-29 16:34:32 -0500

    Update playwright configs

--- playwright.config.ui.dev.ts
+++ playwright.config.ui.dev.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.CI ? 'http://lll-dadabase-web-demo.azurewebsites.net' : 'https://localhost:44349/',
+    baseURL: process.env.CI ? 'http://lll-dadabase-web-dev.azurewebsites.net' : 'https://localhost:44349/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
